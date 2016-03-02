acdec67d67eda32f2ff7c78374f37f9f3a8115de
Akshay Khole <akshay@helloflock.com>
2016-03-02 04:12:46 -0800

    Simple authorization strategy for Rails apps

---
+++ lib/axit.rb
@@ -0,0 +1,41 @@
+module Axit
+  class NotAuthorizedError < StandardError; end
+
+  module Controllers
+    PREFIX = 'Auth::Controllers'.freeze
+
+    private
+
+    def axit!
+      whitelisted?
+    end
+
+    def whitelisted?
+      !!(prefix_string.constantize)
+        .send(action_name, current_user) == true ? true : (raise NotAuthorizedError)
+    end
+
+    def prefix_string
+      # controller name does not give namespace
+      c = params[:controller]
+      "#{PREFIX}::#{c.camelize}"
+    end
+  end
+
+  module Views
+    PREFIX = 'Auth::Views'.freeze
+
+    private
+
+    def can_view?(fragment)
+      !!(prefix_string.constantize)
+        .send(fragment, current_user)
+    end
+
+    def prefix_string
+      c = params[:controller]
+      a = params[:action]
+      "#{PREFIX}::#{c.camelize}::#{a.camelize}"
+    end
+  end
+end
---
+++ test/test_axit.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'axit'
+
+class AxitTest < Minitest::Test
+  def test_prefix_string
+    skip
+  end
+end
